3860621cfbdd87208d6fa4cd4b436877227d49d8
Adam <bialobrzeski.adam@gmail.com>
2013-11-16 12:37:10 +0100

    Test commitu

--- Laboratorium4Zadanie1/Laboratorium4Zadanie1/Program.cs
+++ Laboratorium4Zadanie1/Laboratorium4Zadanie1/Program.cs
@@ -28,6 +28,7 @@ namespace Laboratorium4Zadanie1
             Console.WriteLine("Panda to najlepszy samochód");
 
             Console.WriteLine("Teraz jednak jest coś lepszego od Pandy");
+            Console.WriteLine("Test commitu");
 
 			Console.Write("Nazwa producenta samochodu ... ? ");
 			NazwaProducenta = Console.ReadLine();
